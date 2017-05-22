b49e57e391745c24cca4c65a3940ee2f7c5eacf3
crisnello <crisnello@gmail.com>
2017-05-22 11:22:28 -0300

    Colocando uma resposta de progresso enquanto carrega as notas !!!!

--- app/src/main/java/com/crisnello/notereader/MainActivity.java
+++ app/src/main/java/com/crisnello/notereader/MainActivity.java
@@ -168,31 +168,37 @@ public class MainActivity extends AppCompatActivity
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+                int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-            listaDeNotas.setVisibility(show ? View.GONE : View.VISIBLE);
-            listaDeNotas.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    listaDeNotas.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
+                listaDeNotas.setVisibility(show ? View.GONE : View.VISIBLE);
+                listaDeNotas.animate().setDuration(shortAnimTime).alpha(
+                        show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        listaDeNotas.setVisibility(show ? View.GONE : View.VISIBLE);
+                    }
+                });
 
-            mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgress.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
-            listaDeNotas.setVisibility(show ? View.GONE : View.VISIBLE);
+                mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+                mProgress.animate().setDuration(shortAnimTime).alpha(
+                        show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+                    }
+                });
+            } else {
+                // The ViewPropertyAnimator APIs are not available, so simply show
+                // and hide the relevant UI components.
+                mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+                listaDeNotas.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        }catch(Exception e){
+            //Caso de qualquer erro, oculta progresso e mostra lista
+            mProgress.setVisibility(View.GONE);
+            listaDeNotas.setVisibility(View.VISIBLE);
         }
     }
 
@@ -564,7 +570,7 @@ public class MainActivity extends AppCompatActivity
         compartilha.putExtra(Intent.EXTRA_TEXT, pConteudo);
 
         startActivity(Intent.createChooser(compartilha, pAssunto));
-        
+
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
